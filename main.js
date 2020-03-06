33ea42e7823fdca8a5caf445470822518d474ae2
moazkhan7 <moaz.khan7@gmail.com>
2020-03-06 18:40:17 +0300

    added timer

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-// add jQuery to html
-// found: https://stackoverflow.com/questions/1140402/how-to-add-jquery-in-js-file
-var script = document.createElement('script');
-script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-
-
 var student = {
     kfupm_id: "",
     email: ""
