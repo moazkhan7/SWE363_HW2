4229f57c9b4a4742647c967eba344d3069860221
moazkhan7 <moaz.khan7@gmail.com>
2020-03-07 23:02:56 +0300

    added buttons and functionality to mcq_checkBox

--- mcq.js
+++ mcq.js
@@ -2,5 +2,20 @@ var currentQuestion = 1
 
 function next() {
     $('#question' + currentQuestion).addClass('hide')
-    $('#question' + ++currentQuestion).removeClass('hide')    
+    $('#question' + ++currentQuestion).removeClass('hide')
+
+    if(currentQuestion > 1)
+        $('#previousbtn').removeClass('hide')
+    if(currentQuestion == 5)
+        $('#nextbtn').addClass('hide')
 };
+
+function previous() {
+    $('#question' + currentQuestion).addClass('hide')
+    $('#question' + --currentQuestion).removeClass('hide')
+
+    if(currentQuestion < 5)
+        $('#nextbtn').removeClass('hide')
+    if(currentQuestion == 1)
+        $('#previousbtn').addClass('hide')
+}
